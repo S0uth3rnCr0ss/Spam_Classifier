11712dee7c5104fa95a9f48e82c53564dc79c2cd
S0uth3rnCr0ss <abhinavbiswal123456789@gmail.com>
2025-06-02 22:11:37 +0530

    change made

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 
 
-nltk.download('punkt', download_dir='nltk_data')
+nltk.download('punkt_tab', download_dir='nltk_data')
 nltk.download('stopwords', download_dir='nltk_data')
 
 
